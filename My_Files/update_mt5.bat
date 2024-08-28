52969da56d97cbe86f0ef7b2248254e7708c1a19
Thilina Gallage <2222619+one-gallage@users.noreply.github.com>
2024-08-28 23:38:45 +1000

    modified

--- My_Files/update_mt5.bat
+++ My_Files/update_mt5.bat
@@ -1,63 +1,85 @@
 
 @echo off
-SETLOCAL
+setlocal
 
 set PARENT_DIR_HOME=%CD%
-echo "Parent directory=%PARENT_DIR_HOME%"
+echo Parent directory = %PARENT_DIR_HOME%
 
 set COPY_DIR_SRC=%CD%\copy_source
-echo "Copy source directory=%COPY_DIR_SRC%"
+echo Source directory = %COPY_DIR_SRC%
 
-CALL :DeleteTemp %PARENT_DIR_HOME%\terminal_neo
-CALL :CopyUpdates %COPY_DIR_SRC%\Indicators  %PARENT_DIR_HOME%\terminal_neo\MQL5\Indicators
-CALL :CopyUpdates %COPY_DIR_SRC%\Experts  %PARENT_DIR_HOME%\terminal_neo\MQL5\Experts
+:: Process the each MT5 directory here
 
-CALL :DeleteTemp %PARENT_DIR_HOME%\terminal_1
-CALL :CopyUpdates %COPY_DIR_SRC%\Indicators  %PARENT_DIR_HOME%\terminal_1\MQL5\Indicators
-CALL :CopyUpdates %COPY_DIR_SRC%\Experts  %PARENT_DIR_HOME%\terminal_1\MQL5\Experts
+::CALL :ProcessMT5 %PARENT_DIR_HOME%\meta5
+CALL :ProcessMT5 %PARENT_DIR_HOME%\terminal_2
+CALL :ProcessMT5 %PARENT_DIR_HOME%\terminal_3
+CALL :ProcessMT5 %PARENT_DIR_HOME%\terminal_4
+CALL :ProcessMT5 %PARENT_DIR_HOME%\terminal_5
+CALL :ProcessMT5 %PARENT_DIR_HOME%\terminal_6
+CALL :ProcessMT5 %PARENT_DIR_HOME%\terminal_7
 
-CALL :DeleteTemp %PARENT_DIR_HOME%\terminal_2
-CALL :CopyUpdates %COPY_DIR_SRC%\Indicators  %PARENT_DIR_HOME%\terminal_2\MQL5\Indicators
-CALL :CopyUpdates %COPY_DIR_SRC%\Experts  %PARENT_DIR_HOME%\terminal_2\MQL5\Experts
-
-CALL :DeleteTemp %PARENT_DIR_HOME%\terminal_3
-CALL :CopyUpdates %COPY_DIR_SRC%\Indicators  %PARENT_DIR_HOME%\terminal_3\MQL5\Indicators
-CALL :CopyUpdates %COPY_DIR_SRC%\Experts  %PARENT_DIR_HOME%\terminal_3\MQL5\Experts
-
-CALL :DeleteTemp %PARENT_DIR_HOME%\terminal_4
-CALL :CopyUpdates %COPY_DIR_SRC%\Indicators  %PARENT_DIR_HOME%\terminal_4\MQL5\Indicators
-CALL :CopyUpdates %COPY_DIR_SRC%\Experts  %PARENT_DIR_HOME%\terminal_4\MQL5\Experts
-
-CALL :DeleteTemp %PARENT_DIR_HOME%\terminal_5
-CALL :CopyUpdates %COPY_DIR_SRC%\Indicators  %PARENT_DIR_HOME%\terminal_5\MQL5\Indicators
-CALL :CopyUpdates %COPY_DIR_SRC%\Experts  %PARENT_DIR_HOME%\terminal_5\MQL5\Experts
-
-CALL :DeleteTemp %PARENT_DIR_HOME%\terminal_6
-CALL :CopyUpdates %COPY_DIR_SRC%\Indicators  %PARENT_DIR_HOME%\terminal_6\MQL5\Indicators
-CALL :CopyUpdates %COPY_DIR_SRC%\Experts  %PARENT_DIR_HOME%\terminal_6\MQL5\Experts
-
-CALL :DeleteTemp %PARENT_DIR_HOME%\terminal_7
-CALL :CopyUpdates %COPY_DIR_SRC%\Indicators  %PARENT_DIR_HOME%\terminal_7\MQL5\Indicators
-CALL :CopyUpdates %COPY_DIR_SRC%\Experts  %PARENT_DIR_HOME%\terminal_7\MQL5\Experts
 
 EXIT /B %ERRORLEVEL%
 
+
+:ProcessMT5
+
+if exist "%~1" (
+
+	echo ------------------------------------------
+	echo 	MT5 directory = %~1
+	CALL :DeleteBases %~1
+	CALL :DeleteTemp %~1
+	CALL :CopyUpdates %COPY_DIR_SRC%\Indicators  %~1\MQL5\Indicators
+	CALL :CopyUpdates %COPY_DIR_SRC%\Experts  %~1\MQL5\Experts
+
+)
+
+EXIT /B 0
+
+:DeleteBases
+
+:: Loop through all subdirectories in the base directory
+for /d %%D in ("%~1\Bases\*") do (
+	
+	echo 	cleaning directory = %%D
+	
+	if exist "%%D\history" (
+		RMDIR "%%D\history" /S /Q		
+		PING localhost -n 2 >NUL
+	)
+	
+	if exist "%%D\ticks" (
+		RMDIR "%%D\ticks" /S /Q		
+		PING localhost -n 2 >NUL
+	)
+
+	if exist "%%D\trades" (
+		RMDIR "%%D\trades" /S /Q		
+		PING localhost -n 2 >NUL
+	)
+	
+)
+
+EXIT /B 0
+
 :DeleteTemp
 
-echo "-------------------------------- MT5 directory=%~1"
-
 if exist "%~1\logs" (
-	DEL "%~1\logs\*.*" /S /Q
+	echo 	deleting directory = %~1\logs
+	RMDIR "%~1\logs" /S /Q	
 	PING localhost -n 2 >NUL
 ) 
 
-if exist "%~1\MQL5\logs" (
-	DEL "%~1\MQL5\logs\*.*" /S /Q
+if exist "%~1\MQL5\Logs" (
+	echo 	deleting directory = %~1\MQL5\Logs
+	RMDIR "%~1\MQL5\Logs" /S /Q	
 	PING localhost -n 2 >NUL
 ) 
 
 if exist "%~1\Tester" (
-	RMDIR "%~1\Tester" /S /Q
+	echo 	deleting directory = %~1\Tester
+	RMDIR "%~1\Tester" /S /Q	
 	PING localhost -n 2 >NUL
 ) 
 
@@ -67,7 +89,8 @@ if exist "%~1\MQL5\Profiles\Tester" (
 ) 
 
 if exist "%~1\Bases\Custom" (
-	RMDIR "%~1\Bases\Custom" /S /Q
+	echo 	deleting directory = %~1\Bases\Custom
+	RMDIR "%~1\Bases\Custom" /S /Q	
 	PING localhost -n 2 >NUL
 )
 
@@ -75,16 +98,25 @@ EXIT /B 0
 
 :CopyUpdates
 
-if exist "%~2\Tilly" (
-	RMDIR "%~2\Tilly" /S /Q
-	echo "deleted directory=%~2\Tilly"
-	PING localhost -n 2 >NUL
-) 
+if exist "%~1" (
 
-if exist "%~1\Tilly" (
-	XCOPY %~1 %~2 /E
-	echo "copied directory=%~1\Tilly to the directory=%~2"
-	PING localhost -n 2 >NUL
-) 
+	if exist "%~2\Tilly" (
+		echo 	deleting directory = %~2\Tilly
+		RMDIR "%~2\Tilly" /S /Q		
+		PING localhost -n 2 >NUL
+	) 
+
+	if exist "%~1\Tilly" (
+		echo 	copying directory = %~1\Tilly to the directory = %~2
+		XCOPY %~1 %~2 /E		
+		PING localhost -n 2 >NUL
+	) 
+
+)
+
 
 EXIT /B 0
+
+endlocal
+pause
+
